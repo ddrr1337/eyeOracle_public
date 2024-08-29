6bbc84d52e9f5b9fb97d597643f73dd43867a5e2
ddrr1337 <incaelum@hotmail.com>
2024-08-29 20:17:57 +0200

    logs

--- scripts/eventListener.js
+++ scripts/eventListener.js
@@ -2,14 +2,24 @@ require("dotenv").config();
 const { network, ethers } = require("hardhat");
 const { requestQueue } = require("./queue");
 const { networkConfig } = require("../helper-hardhat-config");
+const winston = require("winston");
+
+const logger = winston.createLogger({
+  level: "info",
+  format: winston.format.combine(
+    winston.format.timestamp({ format: "YYYY-MM-DD HH:mm:ss" }),
+    winston.format.printf(
+      (info) =>
+        `${info.timestamp} [${info.level.toUpperCase()}]: ${info.message}`
+    )
+  ),
+  transports: [
+    new winston.transports.Console(),
+    new winston.transports.File({ filename: "worker.log" }),
+  ],
+});
 
 async function main() {
-  const now = new Date();
-  const formattedDate = now.toDateString(); // "Mon Aug 29 2024"
-  const formattedTime = now.toLocaleTimeString("en-US"); // "5:30:15 PM"
-
-  const dateTime = `[${formattedDate} | ${formattedTime}]`;
-
   const chainId = network.config.chainId;
   const oracleRouterAddress = networkConfig[chainId].ORACLE_ROUTER_ADDRESS;
 
@@ -29,9 +39,7 @@ async function main() {
 
   // Escucha eventos
   oracleRouter.on("OracleRequestHttp", async (requestId, consumer, request) => {
-    console.log(
-      `${dateTime} Event detected: RequestId ${requestId.toString()}`
-    );
+    logger.info(`Event detected: RequestId ${requestId.toString()}`);
 
     await requestQueue.add({
       requestId: requestId.toString(),
@@ -40,7 +48,7 @@ async function main() {
     });
   });
 
-  console.log(`${dateTime} Listening for OracleRequestHttp events...`);
+  logger.info(` Listening for OracleRequestHttp events...`);
 }
 
 main().catch((error) => {
