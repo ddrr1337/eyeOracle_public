2b7c1af35a7bca07307d20d78af587835eafafbc
ddrr1337 <incaelum@hotmail.com>
2024-10-16 16:33:35 +0200

    test example

--- testServer.js
+++ testServer.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
-const PORT = 4500;
+const PORT = 4000;
 
 // Middleware to parse JSON request bodies
 app.use(express.json());
